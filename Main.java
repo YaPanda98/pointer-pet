dd7e784ab23967cc16931c6015c8c87348aaa976
YaPanda98 <52866775+YaPanda98@users.noreply.github.com>
2019-07-29 11:25:18 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,7 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- Dog Mydog = new Dog (); {
+         Dog Mydog = new Dog (); {
 	 Cat myCat = new Cat();
 	 Cow myCow = new Cow ();
 
